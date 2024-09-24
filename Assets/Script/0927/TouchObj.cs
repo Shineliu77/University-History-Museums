8cd8dfc4affd0e8c70c1b677da3189ddc85bbab5
Shineliu77 <shinecookie@gmail.com>
2024-09-24 23:46:03 +0800

    修正模型與影片物件
    修正模型與影片物件

--- Assets/Script/0927/TouchObj.cs
+++ Assets/Script/0927/TouchObj.cs
@@ -19,7 +19,7 @@ public class TouchObj : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(IsUIOpen);
     }
     private void OnMouseDown()
     {
